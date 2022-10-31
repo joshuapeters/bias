13f242c8235d1a59c51890c6ac87430445ed025e
JP <jp@JPs-MacBook-Air.local>
2022-10-31 18:12:05 -0400

    added auth

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Navbar } from '../components/layout/navbar'
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
-        <Navbar />
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
-import { trpc } from "../utils/trpc";
 import { CompactPhotocard } from "../components/photocard";
-import { Navbar } from "../components/layout/navbar";
-import { randomUUID } from "crypto";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const { data: sessionData } = useSession();
+  const loggedIn = sessionData != null;
 
   // TODO: get this from the DB
   const cards = [
@@ -65,51 +62,34 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+        <Greeting name={sessionData?.user?.name} visible={loggedIn} />
         <div className="mt-3 grid gap-5 pt-3 text-center md:grid-cols-3 lg:w-2/3">
           {
-            cards.map(({ name, group, album, pcImageUrl, id }) =>
-              <CompactPhotocard
-                key={id}
-                name={name}
-                artist={group}
-                album={album}
-                pcImageUrl={pcImageUrl}
-                id={id}
-              />
-            )
+            !loggedIn ?
+              <p>Please log in!</p> :
+              cards.map(({ name, group, album, pcImageUrl, id }) =>
+                <CompactPhotocard
+                  key={id}
+                  name={name}
+                  artist={group}
+                  album={album}
+                  pcImageUrl={pcImageUrl}
+                  id={id}
+                />
+              )
           }
         </div>
-        {/* <AuthShowcase /> */}
       </main>
     </>
   );
 };
 
-export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-  const { data: sessionData } = useSession();
-
-  console.log("session: ", sessionData);
-
+const Greeting: React.FC<{ name: string | null | undefined, visible: boolean }> = ({ name, visible }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.name}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
+    <>
+      {visible ? <p>Hello, {name ?? "Friend"}!</p> : null}
+    </>
   );
-};
+}
+
+export default Home;
